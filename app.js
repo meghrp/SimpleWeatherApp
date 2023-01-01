2a6a9bb23b15a8f427d1eea103684f557efa8ab9
Megh Pathak <meghrp27@gmail.com>
2023-01-01 16:20:11 -0500

    Working weather app

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ window.addEventListener('load', () => {
     let temp_description = document.querySelector('.temp-desc');
     let deg = document.querySelector('.degree');
     let timezone = document.querySelector('.location-timezone');
+    const temp_section = document.querySelector('.degree-section');
+    const temp_span = document.querySelector('.degree-section span');
 
     if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
@@ -16,15 +18,34 @@ window.addEventListener('load', () => {
                     return response.json();
                 })
                 .then(data => {
-                    deg.textContent = Math.round(data.main.temp);
+                    // console.log(data);
+                    let temp = Math.round(data.main.temp);
+                    deg.textContent = temp;
                     temp_description.textContent = data.weather[0].description;
                     timezone.textContent = data.name;
+
+                    // Set Icons
+                    setIcons(data.weather[0].icon, document.querySelector('.icon'));
+
+                    // Change temperature to Celsius/Fahrenheit
+                    temp_section.addEventListener('click', () => {
+                        if(temp_span.textContent === "C") {
+                            temp_span.textContent = "F";
+                            deg.textContent = Math.round((data.main.temp * 9/5) + 32);
+                        } else {
+                            temp_span.textContent = "C";
+                            deg.textContent = temp;
+                        }
+                    });
                 });
         });
     } else{
         h1.textContent = "Hey, this is not working because you don't have geolocation enabled on your browser."
     }
 
-    
+    function setIcons(icon, iconID) {
+        iconID.innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}@2x.png">`;
+        return iconID;
+    }
 
 });
